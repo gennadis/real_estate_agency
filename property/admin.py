75b43962b7a0ef6d73781d532c8633cb92f5dc55
Gennady Badinov <gennady.badinov@gmail.com>
2022-02-12 11:42:04 +0300

    Normalize all `Flat.owners_phonenumber`

--- property/admin.py
+++ property/admin.py
@@ -7,7 +7,15 @@ from .models import Flat, Claim
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ("town", "address")
     readonly_fields = ("created_at",)
-    list_display = ("address", "price", "is_new_building", "construction_year", "town")
+    list_display = (
+        "address",
+        "price",
+        "is_new_building",
+        "construction_year",
+        "town",
+        "owners_phonenumber",
+        "owner_pure_phone",
+    )
     list_editable = ("is_new_building",)
     list_filter = ("is_new_building", "rooms_number", "has_balcony")
     raw_id_fields = ("liked_by",)
